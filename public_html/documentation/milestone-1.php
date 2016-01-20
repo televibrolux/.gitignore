a69089d7a8c0db625e3e7256ab10b89b1e2afe28
Duane Ulibarri <dulibarri10@cnm.edu>
2016-01-20 00:35:33 -0700

    making corrections

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -17,7 +17,18 @@
 				The tele has a very bright and punchy tone that is very distinct. The tele can be heard in countless country and jazz records.
 				I was five years old when I heard Buck Owens guitarist chicken pick for the first time and I was hooked.
 			</p2>
-
+			<h2>Brief History</h2>
+		<p>Rockabilly music began in 1954 when Sam Phillips assembled Bill Black on bass, Scotty Moore on guitar and Elvis Presley
+		on vocals and rthym guitar. Those three created magic that would forever change music, especially the lead guitar. Scotty Moore
+		would play the tune and the bass at the same time. When he played it sounded like there were two guitars. There was no overdubbing
+		in those days, the sound was quite extrordinary to say the least. Everyone wanted to know what kind of guitar Scotty was playing.
+		They wanted to recreate the sound they were hearing over the radio. Scotty Moore preferred Gibson guitars because of their mellow
+		sound. Another guitar that was used was a Grestch. It was similar to Gibson guitars but both guitars had unique tones. Another band in \
+		California, Gene Vincent and the Blue Caps were also making waves. His guitarist Cliff Gallup was playing a Fender Telecaster.
+		The Telecaster's tone was not warm at all. It was a very bright (piercing) guitar. The Telecaster became the guitar of choice
+		for rockabilly bands because of their sound and because they were so inexpensive compared to a Gibson or Grestch guitar. This was taking
+		place in 1955. All of the young kids wanted to sound like Cliff Gallup so they bought Fender Telecaster guitars.
+		</p>
 	</body>
 </html>
 
