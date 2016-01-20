652e211471f131b3c0f3b667a1e026d27dce0c3f
Duane Ulibarri <dulibarri10@cnm.edu>
2016-01-20 00:01:50 -0700

    changed content

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Just Me!</title>
+	</head>
+	<body>
+		<h1>Rockabilly Guitar</h1>
+		<p1>I'm going to write a little bit about me, mainly my hobby and passion, music!! I play bluegrass guitar, dobro, pedal steel guitar
+			and the mandolin. I'm also a Fender Telecaster Chickin' Picker! The style of music that I play is bluegrass, country and
+			rockabilly in the style of Scotty Moore. Scotty Moore was Elvis Presley's guitar player at his Sun Session Studios where he recorded,
+			"That's All Right Mama".</p>
+			<h2>My Influences</h2>
+			<p2>My influences in the acoustic guitar are Doc Watson and Clarance White. They were both
+				bluegrass pickers. In the electric guitar my influences are many. They include Paul Burilson, Don Rich, Cliff Gallup and
+				Danny Gatton. What they all have in common is the guitar they play, the Fender Telecaster guitar. The teleaster is
+				a versatile guitar that can be played in many styles including country, jazz, shredding and of course chicken pickin'.
+				The tele has a very bright and punchy tone that is very distinct. The tele can be heard in countless country and jazz records.
+				I was five years old when I heard Buck Owens guitarist chicken pick for the first time and I was hooked.
+			</p2>
+
+	</body>
+</html>
+
