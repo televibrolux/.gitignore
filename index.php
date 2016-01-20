c4998614ef64c7289c6e5468b12bf2aa70456475
Duane Ulibarri <dulibarri10@cnm.edu>
2016-01-19 23:39:45 -0700

    Added more info

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Rockabilly Guitar</title>
+		<h1>Rockabilly Guitar</h1>
 	</head>
 	<body>
-		<p>Grestch</p>
+		<h2>Purpose</h2>
+		<p>I would like for this website to inform fans of Rockabilly music to learn more about the guitars that they played.</p>
+		<h2>Audience</h2>
+		<p>This website is intended for fans around the world that love Rockabilly music.</p>
+		<h2>Goal</h2>
+		<p>The goal of this website is to let fans know the specific guitars that they played that created the sound.</p>
 	</body>
 </html>
